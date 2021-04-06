19613d48d01bc9744f796fff0487b5414f88f0f5
Andrew Reynolds <andrew_reynolds@mac.com>
2021-04-05 21:46:19 -0700

    added event listeners to backdrop and no button

--- shared.js
+++ shared.js
@@ -1,3 +1,29 @@
 const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const selectPlanButtons = document.querySelectorAll(".plan button");
+const noButton = document.querySelector(".modal__actions button");
 
-backdrop.style.display = "block";
+// Functions //
+
+// Close function
+
+const closeModal = () => {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+};
+
+// Event Listeners //
+
+// choose plan buttons
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", () => {
+    backdrop.style.display = "block";
+    modal.style.display = "block";
+  });
+}
+
+// backdrop (close)
+backdrop.addEventListener("click", closeModal);
+
+// modal no button (close)
+noButton.addEventListener("click", closeModal);
